cd6167acab3ade9dbb44e4550426c84ba32f9b6a
citenaresh <73706869+citenaresh@users.noreply.github.com>
2021-04-17 18:29:16 +0800

    Updates

---
+++ precache-manifest.e32158970988ca5246c2e1e789b6ed86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03fc3a7baf3c380462c26e2ad0071aca",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "640b4c1ecc0051ceb3da",
+    "url": "/employee-directory/static/css/main.bc744926.chunk.css"
+  },
+  {
+    "revision": "3055f542ee9a99b26f0b",
+    "url": "/employee-directory/static/js/2.841b6981.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/employee-directory/static/js/2.841b6981.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "640b4c1ecc0051ceb3da",
+    "url": "/employee-directory/static/js/main.0209329b.chunk.js"
+  },
+  {
+    "revision": "79d7c4f4ad2a4d373d4e",
+    "url": "/employee-directory/static/js/runtime-main.759497ab.js"
+  }
+]);
